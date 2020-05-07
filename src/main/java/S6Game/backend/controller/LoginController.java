f7ccbc624b76241b090385adf2529f89e224dbc5
Michelle Broens <michellebroens96@gmail.com>
2020-05-07 15:46:49 +0200

    Register working

--- src/main/java/S6Game/backend/controller/LoginController.java
+++ src/main/java/S6Game/backend/controller/LoginController.java
@@ -3,24 +3,24 @@ package S6Game.backend.controller;
 import S6Game.backend.model.User;
 import S6Game.backend.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/login")
 public class LoginController
 {
 	@Autowired
 	private LoginRepository loginRepository;
+	private Gson gson;
 
 	@PostMapping(path ="/register")
-	public boolean register(User user)
+	public User register(@RequestBody User user)
 	{
-		User createdUser = null;
-		if (loginRepository.findByUsernameNotLike(user.getUsername()) == null) {
-			createdUser = loginRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
-		}
-		return true;
+		loginRepository.save(user);
+		return user;
 	}
 }
