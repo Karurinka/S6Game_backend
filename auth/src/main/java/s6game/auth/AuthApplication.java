e9e95671fa16ccad5472467de296fe7b5a35b8a5
Michelle Broens <michellebroens96@gmail.com>
2020-06-16 16:40:20 +0200

    Fixed dockerized databases and rabbitMQ

---
+++ auth/src/main/java/s6game/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package s6game.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
