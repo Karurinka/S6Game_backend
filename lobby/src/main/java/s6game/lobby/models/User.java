8d49de25571a01e2ba37e79a7aa82aa8036a0c55
Michelle Broens <michellebroens96@gmail.com>
2020-06-21 23:08:06 +0200

    fix error stuff hopefully

--- lobby/src/main/java/s6game/lobby/models/User.java
+++ lobby/src/main/java/s6game/lobby/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String username;
