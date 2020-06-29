80774d9f783ea8a1e66d87caacfdbce3353dbc11
Michelle Broens <michellebroens96@gmail.com>
2020-06-29 02:03:43 +0200

    Changed FAAS get to POST

--- lobby/src/main/java/s6game/lobby/controller/CurseController.java
+++ lobby/src/main/java/s6game/lobby/controller/CurseController.java
@@ -1,8 +1,8 @@
 package s6game.lobby.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/curse")
 public class CurseController {
 
-    @GetMapping("/{message}")
+    @PostMapping("/{message}")
     public String getCensoredMessage(@PathVariable String message){
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject("http://localhost:7071/api/CurseRemover" + message, String.class);
